f4e28e2bfc4b9281adaa99c573aeb805458003f3
Abinash <abinashsahu1805@gmail.com>
2024-07-26 16:27:13 +0530

    update environment

--- CRM/src/Utils/config.js
+++ CRM/src/Utils/config.js
@@ -1,7 +1,7 @@
-export const API = process.env.REACT_APP_API_URL;
-export const JWT_SECRET = process.env.REACT_APP_JWT_SECRET;
-export const SERVER_ROUTE = process.env.REACT_APP_SERVER_ROUTE;
-export const API_URL = process.env.REACT_APP_API_URL;
-export const busKey = 'sidebarBuses';
-export const peopleKey = 'sidebarPeople';
-export const jwtKey = 'jwt';
+export const API = "http://localhost:8525/api";
+export const JWT_SECRET = "123456789";
+export const SERVER_ROUTE = "http://localhost:8525";
+export const API_URL = "http://localhost:8525/api";
+export const busKey = "sidebarBuses";
+export const peopleKey = "sidebarPeople";
+export const jwtKey = "jwt";
